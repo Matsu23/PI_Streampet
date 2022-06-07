d618021ce6e8d171f0d2807db990389f0b3831a5
Matsu23 <matsu23@protonmail.com>
2022-06-07 16:08:32 -0300

    continuacao do desenvolvimento, issue:2 conexoes abertas cm mysql

--- StreamPet/StreamPet/Controllers/LoginController.cs
+++ StreamPet/StreamPet/Controllers/LoginController.cs
@@ -1,10 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using StreamPet.Models;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql;
+using MySql.Data.MySqlClient;
 
 namespace StreamPet.Controllers
 {
     public class LoginController : Controller
+
+        
     {
+
+        
         public IActionResult Index()
         {
             return View();
@@ -12,16 +19,28 @@ namespace StreamPet.Controllers
 
         [HttpPost]
 
-        public IActionResult Logar(LoginModel loginModel)
+        public async Task<IActionResult> Logar(LoginModel loginModel)
         {
+            MySqlConnection mySqlCon = new MySqlConnection("server=localhost;initial catalog=StreamPetDB;uid=root;pwd=senha123");
+            await mySqlCon.OpenAsync();
+            MySqlCommand mySqlComd = mySqlCon.CreateCommand();
+            mySqlComd.CommandText = $"SELECT * FROM usuarios WHERE Email = '{loginModel.Login}' AND Senha = '{loginModel.Senha}'  ";
+            MySqlDataReader reader = mySqlComd.ExecuteReader();
+
             try
             {
 
                 if (ModelState.IsValid)
                 {
-                    if(loginModel.Login == "adm@adm.com" && loginModel.Senha == "123")
+                    if(mySqlComd.ExecuteNonQuery() > 0)
                     {
-                        return RedirectToAction("testelogin", "Home");
+                        while(await reader.ReadAsync())
+                        {
+                            return RedirectToAction("testelogin", "Home");
+
+                        }
+
+                        //return RedirectToAction("testelogin", "Home");
                     }
                     TempData["ErrorMessage"] = $"$Usuario e/ou senha invalidos!";
 
