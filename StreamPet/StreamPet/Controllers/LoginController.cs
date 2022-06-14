ddc286a38efe4a687b952e81dd839897d3f92c93
Matsu23 <matsu23@protonmail.com>
2022-06-13 23:39:25 -0300

    query/verificacao de login

--- StreamPet/StreamPet/Controllers/LoginController.cs
+++ StreamPet/StreamPet/Controllers/LoginController.cs
@@ -11,34 +11,46 @@ namespace StreamPet.Controllers
         
     {
 
+        private readonly DataContext _context;
+
+        public LoginController(DataContext context)
+        {
+            _context = context;
+        }
+
         
         public IActionResult Index()
         {
             return View();
         }
 
+        public DataContext Get_context()
+        {
+            return _context;
+        }
+
         [HttpPost]
 
         public async Task<IActionResult> Logar(LoginModel loginModel)
         {
-            MySqlConnection mySqlCon = new MySqlConnection("server=localhost;initial catalog=StreamPetDB;uid=root;pwd=senha123");
-            await mySqlCon.OpenAsync();
-            MySqlCommand mySqlComd = mySqlCon.CreateCommand();
-            mySqlComd.CommandText = $"SELECT * FROM usuarios WHERE Email = '{loginModel.Login}' AND Senha = '{loginModel.Senha}'  ";
-            MySqlDataReader reader = mySqlComd.ExecuteReader();
+            var logins =  _context.Usuarios;
 
-            try
-            {
+
+            try {
+
+                var query = logins.Where(x => x.Email == loginModel.Login && x.Senha == loginModel.Senha).ToListAsync();
+                
 
                 if (ModelState.IsValid)
                 {
-                    if(mySqlComd.ExecuteNonQuery() > 0)
+                    if(query.Result.Count > 0)
                     {
-                        while(await reader.ReadAsync())
-                        {
+                        
+
+         
                             return RedirectToAction("testelogin", "Home");
 
-                        }
+                        
 
                         //return RedirectToAction("testelogin", "Home");
                     }
